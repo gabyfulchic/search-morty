139fe55df5da08b4acb889fcb8d16a5f8aad905b
gfulchic <gfulchic@deezer.com>
2020-03-12 12:55:38 +0100

    ADD - packages that i need

--- main.py
+++ main.py
@@ -3,5 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return "This is the Index Page ^^"
+
+@app.route("/test_route")
+def troll():
+    message = "This a test to validate flask behaviour."
+    return message
+
+if __name__ == '__main__':
+    context = ('search-morty.crt', 'search-morty.key')
+    app.run(debug=True, ssl_context=context)
