02efab177a60f2ebeaba6ad0a68493cd919b58c7
Alexandre Abruzzese <alexa@Alexandres-MacBook-Pro.local>
2020-03-13 10:30:25 +0100

    revert

--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,37 @@
-import os
-import wtforms as wt
-import json
 
-from wtforms import TextField, Form
+import os
 from ramapi import *
-from flask import Flask, Response, render_template, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-NAMES = ["abc","abcd","abcde","abcdef"]
-
-class SearchForm(Form):
-    autocomp = TextField('autocomp', id = 'autocomplete')
 
 def create_first_page():
     app = Flask(__name__)
     Bootstrap(app)
-    return app
 
+    return app
 
 def get_all_characters():
     return ramapi.Character.get_all().get("results", "")
 
+@app.route("/")
+def home():
+    return render_template('index.html')
 
-@app.route('/autocomplete', methods=['GET'])
-def autocomplete():
-    search = request.args.get('term')
-
-    app.logger.debug(search)
-    return Response(json.dumps(NAMES), mimetype='application/json')
-
-
-@app.route("/", methods=['GET', 'POST'])
-def index():
+@app.route("/", methods=['POST'])
+def my_form_post():
     find = 0
-    form = SearchForm(request.form)
+    character_to_search = request.form['character']
 
-    if (request.method == "POST" and form.validate()):
+    if (character_to_search == ""):
+        return "Merci de saisir un personnage."
+    else:
         all_characters = get_all_characters()
         for c in all_characters:
-            if (form == c.get("name", "")):
+            if (character_to_search == c.get("name", "")):
                 find = 1
                 break
         if (find == 1):
             return "Nous avons trouvé votre personnage : " + character_to_search
-        else:
-            return "Désolé, nous n'avons pas trouvé votre personnage."
-    else:
-        return render_template("index.html", form = SearchForm(request.form))
 
-    return render_template("index.html", form = character_to_search)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return "Désolé, nous n'avons pas trouvé votre personnage."
